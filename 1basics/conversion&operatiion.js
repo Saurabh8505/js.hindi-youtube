f1ccb2c8cbcd4d2c4f0ddaf68b001fe8b2c96b69
Saurabh Mishra <103993107+Saurabh8505@users.noreply.github.com>
2024-01-01 07:56:32 +0000

    .

---
+++ 1basics/conversion&operatiion.js
@@ -0,0 +1,12 @@
+let score = 0
+
+console.log(typeof (score))
+ let valueInNumber = Number(score)
+ console.log(typeof (valueInNumber)) 
+ console.log(valueInNumber) 
+ let valueInboolean = Boolean(score)
+ console.log(typeof (valueInboolean)) 
+ console.log(valueInboolean) 
+ let valueInstring = String(score)
+ console.log(typeof (valueInstring)) 
+ console.log(valueInstring) 
