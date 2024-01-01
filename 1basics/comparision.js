9aae7669570728d0aa635506f92b94c536c82e43
Saurabh Mishra <103993107+Saurabh8505@users.noreply.github.com>
2024-01-01 14:30:16 +0000

    other

--- 1basics/comparision.js
+++ 1basics/comparision.js
@@ -1,18 +1,29 @@
-let a = 1
-let b = 12
-let c = 7
 
-let max;
-if(a > b && a > c){
-    max = a;
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(undefined <= 0);
 
-}
-else if(b > a && b > c){
-    max = b;
+console.log(true == 0);
+console.log("2" === 2);
+console.log(true === 1);
+
+//greatest number
+// let a = 1
+// let b = 12
+// let c = 7
+
+// let max;
+// if(a > b && a > c){
+//     max = a;
+
+// }
+// else if(b > a && b > c){
+//     max = b;
     
-}
-else{
-    max = c;
+// }
+// else{
+//     max = c;
    
-}
-console.log(max)
+// }
+// console.log(max)
