c527af548487dc14b988a09e66c971621224ac73
Saurabh Mishra <103993107+Saurabh8505@users.noreply.github.com>
2024-01-01 08:34:06 +0000

    operations

---
+++ 1basics/operation.js
@@ -0,0 +1,11 @@
+// let str1 = "Saurabh"
+// let str2 = " Mishra"
+// console.log(str1)
+// console.log(str2)
+// console.log(str1 + str2)
+let gamecounter = 100
+console.log(gamecounter)
+let youNeed = ++gamecounter
+// ++gamecounter;
+console.log(gamecounter)
+console.log(youNeed)
