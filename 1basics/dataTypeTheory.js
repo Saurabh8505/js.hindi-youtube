013ea8fb60ccda9bae356385c63b01fa79068508
Saurabh Mishra <103993107+Saurabh8505@users.noreply.github.com>
2024-01-01 14:52:08 +0000

    .

---
+++ 1basics/dataTypeTheory.js
@@ -0,0 +1,37 @@
+/*
+# Primitive Data type
+
+7 types : String,Number, boolean,Null,Undefined,Symbol,BigInt
+const city = "Bhopal"
+let age = 22
+const male = true
+
+ const outsideTemp = null
+ let useEmail;
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+ const BigNumber  = 2312421232144343241232n
+
+# Reference Data Type(Non primitive)
+
+Arrays, Objects, Functions
+
+const heros = ["Shaktiman","Naagarjuna","Krish"]
+
+let myObj = {
+    name: "Saurabh",
+    age:22;
+}
+
+const  myfunction = function(){
+  console.log("hello World")
+}
+
+to find datatype use 
+typeof
+
+let age = 22
+eg console.log(typeof(age))
+o/p = number
+*/
