c9e8cc2583b9d78d0d4b1139c294917351ad97c2
Saurabh Mishra <103993107+Saurabh8505@users.noreply.github.com>
2024-01-06 07:44:01 +0000

    maths

---
+++ nums_maths.js
@@ -0,0 +1,39 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const dec = 122.5
+// console.log(dec.toPrecision(3));
+
+// const mil = 1000000
+// console.log(mil.toLocaleString('en-IN'));
+
+//+++++++++++++++++++ MATHS +++++++++++++++++++++++
+
+//console.log(Math.abs(-4));
+
+//console.log(Math.PI);
+
+//console.log(Math.round(4.556));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.2));
+
+//console.log(Math.sqrt(169));  //13
+
+//console.log(Math.pow(4 , 3)); //64
+// console.log(Math.min(2 ,3,45,6));
+// console.log(Math.max(2 ,3,45,6));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10 + 1));
+
+//Dice
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
