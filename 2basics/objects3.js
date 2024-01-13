16639d05ae32f5ab4bb35dbee7657e6d145ae0de
Saurabh Mishra <103993107+Saurabh8505@users.noreply.github.com>
2024-01-13 08:42:55 +0000

    new object

---
+++ 2basics/objects3.js
@@ -0,0 +1,10 @@
+const course = {
+    courseName: "Js in Hindi",
+    coursePrice: 999,
+    courseInstructure: "Saurabh"
+}
+// console.log(course["courseInstructure"]);
+
+const {courseInstructure: instructure} = course
+console.log(instructure);
+
--- 2basics/ojects2.js
+++ 2basics/ojects2.js
@@ -1,30 +1,35 @@
 //singleton
 
-const tinderUser = new Object()
+// const tinderUser = new Object()
 
-tinderUser.id = "123abs"
-tinderUser.name = "Saurabh Mishra"
-tinderUser.age = 24
-tinderUser.status = "Single"
+// tinderUser.id = "123abs"
+// tinderUser.name = "Saurabh Mishra"
+// tinderUser.age = 24
+// tinderUser.status = "Single"
 
-// console.log(tinderUser);
+// // console.log(tinderUser);
 
-const regularUser = {
-    email: "saurabh@google.com",
-    fullName: {
-        firstName: "Saurabh",
-        lastName: "MIshra"
-    },
-    College: {
-        mainGroup: "Oriental Group",
-        CollageName:{
-            Collage1: "Oriental Institute of Technology",
-            Collage2: "Oriental Collage of Technology" 
-        }
-    } ,
-    age: 22
-}
+// const regularUser = {
+//     email: "saurabh@google.com",
+//     fullName: {
+//         firstName: "Saurabh",
+//         lastName: "MIshra"
+//     },
+//     College: {
+//         mainGroup: "Oriental Group",
+//         CollageName:{
+//             Collage1: "Oriental Institute of Technology",
+//             Collage2: "Oriental Collage of Technology" 
+//         }
+//     } ,
+//     age: 22
+// }
 // console.log(regularUser.College);
+
+
+//Merging Object
+
+
 const obj1 = {
     name1:"Saurabh Mishra",
     Collage1: "OCT"
@@ -33,5 +38,10 @@ const obj2={
     name2:"Naruto Uzumaki",
     Collage2:"OIST"
 }
-const newObj = [...obj1,...obj2]
-console.log(newObj);
+
+
+const newObj = {...obj1, ...obj2}
+// const newObj = Object.assign({},obj1,obj2) //{} is target and other obj are source
+console.log(Object.keys(newObj));
+console.log(Object.values(newObj));
+console.log(Object.entries(newObj));
